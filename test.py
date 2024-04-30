e685495ccbed83d76b9542d0043bc8d933a7ad00
FabScott <143505755+FabScott@users.noreply.github.com>
2024-04-30 11:42:02 +0200

    Small name change

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def load_maia_network(elo, lc0_path=None, models_dir_path=None, time_limit=None)
     valid_elos = [str(el) for el in range(1100, 2000, 100)]
     assert str(elo) in valid_elos, f"Invalid elo value: {elo}, model does not exist. Valid elos are: {valid_elos}"
 
-    lc0_path = f"lc0-v0.30.0-windows-gpu-nvidia-cuda/lc0.exe" if lc0_path is None else lc0_path
+    lc0_path = f"lc0-exe-folder/lc0.exe" if lc0_path is None else lc0_path
     models_dir_path = f'move_prediction/model_files/{elo}' if models_dir_path is None else models_dir_path
 
     model, config = load_model_config(config_dir_path=models_dir_path, lc0Path=lc0_path)
