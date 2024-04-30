1b1085799d6eb8e12d48643591a64515d7fb29d3
FabScott <143505755+FabScott@users.noreply.github.com>
2024-04-30 11:53:51 +0200

    Update load_maia.py

--- load_maia.py
+++ load_maia.py
@@ -1,12 +1,12 @@
 
-def load_maia_network(elo, lc0_path=None, models_dir_path=None, time_limit=None):
+def load_maia_network(elo, lc0_path=None, models_dir_path=None, time_limit=None, parent=''):
     from move_prediction.maia_chess_backend import load_model_config
 
     valid_elos = [str(el) for el in range(1100, 2000, 100)]
     assert str(elo) in valid_elos, f"Invalid elo value: {elo}, model does not exist. Valid elos are: {valid_elos}"
 
-    lc0_path = f"lc0-exe-folder/lc0.exe" if lc0_path is None else lc0_path
-    models_dir_path = f'move_prediction/model_files/{elo}' if models_dir_path is None else models_dir_path
+    lc0_path = f"{parent}lc0-exe-folder/lc0.exe" if lc0_path is None else lc0_path
+    models_dir_path = f'{parent}move_prediction/model_files/{elo}' if models_dir_path is None else models_dir_path
 
     model, config = load_model_config(config_dir_path=models_dir_path, lc0Path=lc0_path)
     print('Model loaded successfully!')
